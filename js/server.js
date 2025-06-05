f45df46a83f3bc463b943e87d93e85be76499116
yashpatel00 <yashcpatel4445@gmail.com>
2025-06-05 19:46:05 +0530

    my project

--- js/server.js
+++ js/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://127.0.0.1:8080',
+  origin: 'https://ecom-js-seven.vercel.app',
   credentials: true
 }));
 
